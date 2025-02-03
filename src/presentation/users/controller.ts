eff5378a8603415c48b6822da7d9e8fe0a258b51
Andre Ignorosa <andreiglu@hotmail.com>
2025-02-03 14:36:56 -0600

    feat: add create user use case in user controller

--- src/presentation/users/controller.ts
+++ src/presentation/users/controller.ts
@@ -1,12 +1,36 @@
 import { Request, Response } from "express";
+import { CreateUserDto } from "@/domain/dtos";
+import { CustomError } from "@/domain/errors/custom.error";
+import { UserRepository } from "@/domain/repositories/user.repository";
+import { CreateUser } from "@/domain/use-cases/user/create-user";
 
 
 export class UserController {
-    constructor(){};
+    constructor(
+        private readonly userRepository: UserRepository
+    ){};
+
+    private handleError = (res: Response, error: unknown) => {
+        if (error instanceof CustomError) {
+            res.status(error.statusCode).json({error: error.message});
+            return;
+        };
+        
+        console.log(error);
+        res.status(500).json({error: 'Internal server error'});
+    }
 
     public createUser = (req: Request, res: Response) => {
-        res.json({message: 'create user'});
-        return;
+        const [errorMessages, dto] = CreateUserDto.create(req.body);
+        if (errorMessages) {
+            res.status(400).json({errorMessages});
+            return;
+        };
+
+        new CreateUser(this.userRepository)
+            .execute(dto!)
+            .then(user => res.status(201).json(user))
+            .catch(error => this.handleError(res, error));
     };
 
     public deleteUser = (req: Request, res: Response) => {
--- src/presentation/users/routes.ts
+++ src/presentation/users/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { UserController } from "@presentation/users/controller";
+import { UserDatasourceImpl } from "@/infrastructure/datasources/user.datasource.impl";
+import { UserRepositoryImpl } from "@/infrastructure/repositories/user.repository.impl";
 
 export class UserRoutes {
 
     static get routes(): Router{
         const router = Router();
-        const userController = new UserController();
+        const datasource = new UserDatasourceImpl();
+        const userRepository = new UserRepositoryImpl(datasource);
+        const userController = new UserController(userRepository);
 
         router.post('/signup', userController.createUser);
         router.delete('/deleteUser/:id', userController.deleteUser);
