000b728b8ea83c56d7f1f06115db388667c4b7f6
Andre Ignorosa <andreiglu@hotmail.com>
2025-02-13 13:41:01 -0600

    test: add envs tests

---
+++ tests/config/envs.test.ts
@@ -0,0 +1,25 @@
+// import {envs} from '@src/config/envs';
+import {envs} from '../../src/config/envs';
+
+describe('envs.test ', () => { 
+
+    test('should return envs options', () => { 
+        expect(envs).toEqual({
+            PORT: 3001,
+            PUBLIC_PATH: 'public',
+            JWT_SECRET_SEED: expect.any(String)
+        })
+    });
+
+    test('should return an error if any env is not found', async () => { 
+        jest.resetModules();
+        process.env.PORT = 'abcde';
+
+        try {
+            await import('../../src/config/envs');
+            expect(true).toBeFalsy();
+        } catch (error) {
+            expect(`${error}`).toContain('"PORT" should be a valid integer');
+        }
+    });
+})
