68f069f542f910912ae1715dc48233b0c47df716
Andre Ignorosa <andreiglu@hotmail.com>
2025-02-13 11:57:11 -0600

    fix: validate type of input data in login-user dto

--- src/domain/dtos/user/login-user.dto.ts
+++ src/domain/dtos/user/login-user.dto.ts
@@ -11,9 +11,18 @@ export class LoginUserDto {
         const {username, password} = props;
         let errors: IErrorMsg[] = []; 
         
-        if(!username || username.trim().length === 0) errors.push({field: FIELDS.USERNAME, message: ERROR_MESSAGES.USERNAME.REQUIRED});
-        if(!password || password.trim().length === 0) errors.push({field: FIELDS.PASSWORD, message: ERROR_MESSAGES.PASSWORD.REQUIRED});
+        if(!username) errors.push({field: FIELDS.USERNAME, message: ERROR_MESSAGES.USERNAME.REQUIRED});
+        if(!password) errors.push({field: FIELDS.PASSWORD, message: ERROR_MESSAGES.PASSWORD.REQUIRED});
         
+        if(errors.length > 0) return errors;
+        
+        if(typeof username !== 'string') errors.push({field: FIELDS.USERNAME, message: ERROR_MESSAGES.USERNAME.STRING});
+        if(typeof password !== 'string') errors.push({field: FIELDS.PASSWORD, message: ERROR_MESSAGES.PASSWORD.STRING});
+
+        if(errors.length > 0) return errors;
+
+        if(username.trim().length === 0) errors.push({field: FIELDS.USERNAME, message: ERROR_MESSAGES.USERNAME.BLANK_SPACES});
+
         return errors;
     }
     static create(props: ILoginUser): [IErrorMsg[]?, LoginUserDto?]{
