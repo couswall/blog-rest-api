70923144dcd2ed6a5cbe7f5b03b33c0fb6a0a1c9
Andre Ignorosa <andreiglu@hotmail.com>
2025-05-12 21:35:55 -0600

    test: test get likes by userId and blogId methods in likeController

--- tests/presentation/likes/controller.test.ts
+++ tests/presentation/likes/controller.test.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/data/postgres";
 import { LikeDatasourceImpl } from "@/infrastructure/datasources/like.datasource.impl";
 import { LikeRepositoryImpl } from "@/infrastructure/repositories/like.repository.impl";
 import { LikeController } from "@/presentation/likes/controller";
-import { createDeleteLikeDtoObj, likeObjPrisma, newBlogPrisma, userObjPrisma } from "tests/fixtures";
+import { createDeleteLikeDtoObj, likeObjPrisma, likesByBlogIdPrisma, likesByUserIdPrisma, newBlogPrisma, userObjPrisma } from "tests/fixtures";
 import { LIKE_RESPONSE } from "@/infrastructure/constants/like.constants";
 import { DTO_ERRORS } from "@/domain/constants/dto/like.constants";
 
@@ -17,6 +17,7 @@ jest.mock('@/data/postgres', () => ({
         },
         like: {
             findUnique: jest.fn(),
+            findMany: jest.fn(),
             create: jest.fn(),
             delete: jest.fn(),
         },
@@ -140,6 +141,126 @@ describe('LikeController test', () => {
                 expect(mockResponse.json).toHaveBeenCalledWith({
                     success: false,
                     error: {message: `Blog with id ${createDeleteLikeDtoObj.blogId} does not exist`}
+                });
+            });
+        });
+    });
+    describe('getLikesByBlogId()', () => {
+        test('should return a 200 status and an array of likes', async () => {
+            const blogId = 1;
+            mockRequest.params = {blogId: String(blogId)};
+
+            (prisma.blog.findFirst as jest.Mock).mockResolvedValue(newBlogPrisma);
+            (prisma.like.findMany as jest.Mock).mockResolvedValue(likesByBlogIdPrisma);
+
+            await new Promise<void>((resolve) => {
+                likeController.getLikesByBlogId(mockRequest as Request, mockResponse as Response);
+                setImmediate(resolve);
+            });
+
+            expect(mockResponse.status).toHaveBeenCalledWith(200);
+            expect(mockResponse.json).toHaveBeenCalledWith({
+                success: true,
+                message: `${LIKE_RESPONSE.SUCCESS.LIKES_BY_BLOGID} ${blogId}`,
+                data: expect.any(Array),
+            });
+        });
+        describe('Blog validation', () => {
+            test('should throw a 400 error status if blogId is not sent', async () => {
+                mockRequest.params = {};
+                await likeController.getLikesByBlogId(mockRequest as Request, mockResponse as Response);
+    
+                expect(mockResponse.status).toHaveBeenCalledWith(400);
+                expect(mockResponse.json).toHaveBeenCalledWith({
+                    success: false,
+                    error: {message: LIKE_RESPONSE.ERRORS.LIKES_BY_BLOGID.NUMBER}
+                });
+            });
+            test('should throw a 400 error status if blogId is not a number', async () => {
+                mockRequest.params = {blogId: 'abcde'};
+                await likeController.getLikesByBlogId(mockRequest as Request, mockResponse as Response);
+    
+                expect(mockResponse.status).toHaveBeenCalledWith(400);
+                expect(mockResponse.json).toHaveBeenCalledWith({
+                    success: false,
+                    error: {message: LIKE_RESPONSE.ERRORS.LIKES_BY_BLOGID.NUMBER}
+                });
+            });
+            test('should throw a 400 error status if blog with provided ID does not exist', async () => {
+                const blogId = 1;
+                mockRequest.params = {blogId: String(blogId)};
+
+                (prisma.blog.findFirst as jest.Mock).mockResolvedValue(null);
+
+                await new Promise<void>((resolve) => {
+                    likeController.getLikesByBlogId(mockRequest as Request, mockResponse as Response);
+                    setImmediate(resolve);
+                });
+
+                expect(mockResponse.status).toHaveBeenCalledWith(400);
+                expect(mockResponse.json).toHaveBeenCalledWith({
+                    success: false,
+                    error: {message: `Blog with id ${blogId} does not exist`}
+                });
+            });
+        });
+    });
+    describe('getLikesByUserId()', () => {
+        test('should return a 200 status and an array of likes', async () => {
+            const userId = 1;
+            mockRequest.params = {userId: String(userId)};
+
+            (prisma.user.findFirst as jest.Mock).mockResolvedValue(userObjPrisma);
+            (prisma.like.findMany as jest.Mock).mockResolvedValue(likesByUserIdPrisma);
+
+            await new Promise<void>((resolve) => {
+                likeController.getLikesByUserId(mockRequest as Request, mockResponse as Response);
+                setImmediate(resolve);
+            });
+
+            expect(mockResponse.status).toHaveBeenCalledWith(200);
+            expect(mockResponse.json).toHaveBeenCalledWith({
+                success: true,
+                message: `${LIKE_RESPONSE.SUCCESS.LIKES_BY_USERID} ${userId}`,
+                data: expect.any(Array),
+            });
+        });
+        describe('User validation', () => {
+            test('should throw a 400 error status if userId is not sent', async () => {
+                mockRequest.params = {};
+                await likeController.getLikesByUserId(mockRequest as Request, mockResponse as Response);
+    
+                expect(mockResponse.status).toHaveBeenCalledWith(400);
+                expect(mockResponse.json).toHaveBeenCalledWith({
+                    success: false,
+                    error: {message: LIKE_RESPONSE.ERRORS.LIKES_BY_USERID.NUMBER}
+                });
+            });
+            test('should throw a 400 error status if userId is not a number', async () => {
+                mockRequest.params = {userId: 'abcde'};
+                await likeController.getLikesByUserId(mockRequest as Request, mockResponse as Response);
+    
+                expect(mockResponse.status).toHaveBeenCalledWith(400);
+                expect(mockResponse.json).toHaveBeenCalledWith({
+                    success: false,
+                    error: {message: LIKE_RESPONSE.ERRORS.LIKES_BY_USERID.NUMBER}
+                });
+            });
+            test('should throw a 400 error status if user with provided ID does not exist', async () => {
+                const userId = 1;
+                mockRequest.params = {userId: String(userId)};
+
+                (prisma.user.findFirst as jest.Mock).mockResolvedValue(null);
+
+                await new Promise<void>((resolve) => {
+                    likeController.getLikesByUserId(mockRequest as Request, mockResponse as Response);
+                    setImmediate(resolve);
+                });
+
+                expect(mockResponse.status).toHaveBeenCalledWith(400);
+                expect(mockResponse.json).toHaveBeenCalledWith({
+                    success: false,
+                    error: {message: `User with id ${userId} does not exist`}
                 });
             });
         });
